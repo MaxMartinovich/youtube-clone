a7f8baf1dac284a5205c52f5c5cf2ab00b8152f1
MaxMartinovich <max.martinovich@sempreceub.com>
2026-01-02 17:16:12 -0300

    Adicionado que tanto o icone do canal, quanto o nome do canal levem diretamente a pagina do canal

--- index.js
+++ index.js
@@ -11,3 +11,10 @@ document.addEventListener("click", (event) => {
 
   window.location.href = titulo.dataset.link;
 });
+
+document.addEventListener("click", (event) => {
+  const icone = event.target.closest(".icone-canal, .autor-video");
+  if (!icone) return;
+
+  window.location.href = icone.dataset.link;
+});
